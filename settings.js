2d7258955de96a17469243f93bbe46b7fd0c318b
Gareng55 <106025820+Gareng55@users.noreply.github.com>
2022-05-24 00:36:45 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,67 +1,64 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-//---------( APIKEY )---------//
+// Website Api
 global.APIs = {
-zenz: 'https://zenzapis.xyz',
+	alfa: 'https://api.zeeoneofc.xyz', //apabila link api eror, segera laporkan le owner
 }
+
+// Free apikey
 global.APIKeys = {
-'https://zenzapis.xyz': 'BagasPrdn',
+	'https://api.zeeoneofc.xyz': 'your apikey',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
 }
 
-//---------( SETTING )---------//
-global.owner = ['6282279915237','6285789004732','6285724706876']
-global.premium = ['6282279915237']
-global.ownernomer = '6282279915237'
-global.ownername = 'Lexxy Official'
-global.botname = 'KaguraMD'
-global.footer = 'Lexxy Official'
-global.thumb = fs.readFileSync('./image/fake.jpg')
-global.veloriy = fs.readFileSync('./image/kagura.mp4')
+// setting 
+global.autoread = false // auto read pesan / message
+//global.autorecording = true //status auto merekam ( auto record )
+//global.autoketik = false //status auto mengetik (auto typing)
+//global.available = false //status online (online)
+
+// Other
+global.botname = "Aljub-Mdོ" //namabot kalian
+global.ownername= "ᴹᴿ᭄ aljubོ ×፝֟͜×" //nama kalian
+global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
+global.github = "https://zeeone-ofc.github.io/" //bebas
+global.email = "zeeoneofc@gmail.com" //bebas
+global.region = "Indonesia" //bebas
+global.ownernomer = "6283847871545" // nomor wa kalian
+global.ownernomerr = "+6283847871545" //nmr wa kalian
+global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.donasi = "./image/donasi.jpg" // foto donasi di folder image
+global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
+global.owner = ["6283847871545","62887435047326","918156874290"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© Aljub-Mdོ' //sticker wm ubah
+global.author = 'Di Buat Oleh AljubOfc' //sticker wm ganti nama kalian
 global.sessionName = 'session'
-global.packname = '© BotWa'
-global.author = '2022'
-global.sp = '⭔'
-
-//---------( INFO )---------//
-global.youtube = 'https://youtube.com/c/LEX4YOUU'
-global.ig = 'https://instagram.com/Lexxy24'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://github.com/Lexxy24'
-global.email = 'pebrikito0@gmail.com'
-global.region = 'indonesia'
-global.prefa = ['','!','.','🐦','🐤','🗿']
-global.BgWelcLeav = 'https://telegra.ph/file/ca207893ae26d531cd9c6.jpg'
-global.limitawal = {
-premium: "Infinity",
-free: 15,
-}
-
-//---------( RANDOM IMAGE )---------//
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 
-//---------( MESSAGES )---------//
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
 global.mess = {
-success: '_Done._',
-admin: '_Fitur Khusus Admin Group!_',
-botAdmin: '_Bot Harus Menjadi Admin Terlebih Dahulu!_',
-owner: '_Fitur Khusus Owner Bot_',
-group: '_Fitur Digunakan Hanya Untuk Group!_',
-private: '_Fitur Digunakan Hanya Untuk Private Chat!_',
-bot: '_Fitur Khusus Pengguna Nomor Bot_',
-wait: '_Sedang Di Proses..._',
-error: '_Fitur Sedang Error!_',
-endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12_',
+    sukses: '🤗Done, Oke Desu~',
+    admin: 'Perintah Ini Hanya Bisa Digunakan Oleh Admin !',
+    botAdmin: 'Perintah Ini Hanya Bisa Digunakan Ketika Bot Menjadi Admin !',
+    owner: 'Perintah Ini Hanya Bisa Digunakan Oleh Owner !',
+    group: 'Perintah Ini Hanya Bisa Digunakan Di Group !',
+    private: 'Perintah Ini Hanya Bisa Digunakan Di Private !',
+	bot: 'Fitur Khusus Pengguna Nomor Bot',
+    errtoimg: 'Maaf Saat Ini Belum Support Sticker Gif !',
+    wait: '⏳ Sedang Di Proses',
+	lockCmd: 'Fitur Tidak Diaktifkan Oleh Owner!',
+	example1: 'Selamat Datang @user Di Group @subject Jangan Lupa Baca Rules @desc\n\nNote :\n1. @user (Mention User Join)\n2. @bio (Get Bio User Join)\n3. @tanggal (Date Now)\n4. @desc (Get Description Group)\n5. @subject (Group Name)'
 }
 
-//---------( PEMBATAS )---------//
 let file = require.resolve(__filename)
 fs.watchFile(file, () => {
-fs.unwatchFile(file)
-console.log(chalk.redBright(`Update'${__filename}'`))
-delete require.cache[file]
-require(file)
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
 })
